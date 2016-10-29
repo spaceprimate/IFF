757c2ea69394b132265911741e31733bc2ced798
Dan <d4nielmurphy@gmail.com>
2016-10-28 20:58:54 -0500

    pushing to github

---
+++ README.md
@@ -0,0 +1,4 @@
+**Bootstrap theme for International Food Fest**
+Version: 1.0
+
+Contact: Daniel Murphy, d4nielmurphy@gmail.com
---
+++ js/custom.js
@@ -0,0 +1,51 @@
+/*
+ * MAIN JS FILE
+ * 
+ */
+$( document ).ready(function() {
+    var hashTagActive = "";
+    $(".scroll").on("click touchstart" , function (event) {
+        //alert("clicked");
+        if(hashTagActive != this.hash) { //this will prevent if the user click several times the same link to freeze the scroll.
+            event.preventDefault();
+            //calculate destination place
+            var dest = 0;
+            if ($(this.hash).offset().top > $(document).height() - $(window).height()) {
+                dest = $(document).height() - $(window).height();
+            } else {
+                dest = $(this.hash).offset().top;
+            }
+            dest = dest - 60;
+            //go to destination
+            $('html,body').animate({
+                scrollTop: dest
+            }, 300, 'swing');
+            hashTagActive = this.hash;
+        }
+    });
+
+
+    $( "#schedule-content" ).load( "content/schedule.html" );
+    $( "#map-content" ).load( "content/map.html" );
+    $( "#food-content" ).load( "content/food.html" );
+    $( "#about-content" ).load( "content/about.html" );
+    $( "#kids-content" ).load( "content/kids.html" );
+    $( "#sponsors-content" ).load( "content/sponsors.html" );
+    $( "#donations-content" ).load( "content/donations.html" );
+    $( "#contact-content" ).load( "content/contact.html" );
+    //map
+    //food
+    //about
+    //kids
+    //contact
+
+
+    $(function(){ 
+        var navMain = $("#navbar");
+        navMain.on("click", "a", null, function () {
+            navMain.collapse('hide');
+        });
+    });
+});
+
+
